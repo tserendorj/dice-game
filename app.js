ae5515b7258cc920667d495075ec993f31ae42d8
tseye <tserendorj.tumen@gmail.com>
2021-04-25 19:49:29 +0800

    toglogchiin eeljiig solidog bolgono

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Тоглогчийн ээлжийг хадгалах хувьсагч, negdugeer 0, hoyordugaar 1 gj
-var activerPlayer = 1;
+var activerPlayer = 0;
 // Тоглогчдын цуглуулсан оноог хадгалах хувьсагч
 var scores = [0, 0];
 // Тоглогчийн ээлжиндээ цуглуулж байгаа оноог хадгалах хувьсагч
@@ -15,11 +15,44 @@ document.getElementById('current-1').textContent = '0';
 
 var diceDom = document.querySelector('.dice');
 diceDom.style.display = "none";
+// dice roller event listener
 
 document.querySelector('.btn-roll').addEventListener('click', function(){
+    // random number between 1 and 6 
     var diceNumber = Math.floor(Math.random() * 6) + 1;
+    // display the picture of dice which default
     diceDom.style.display = "block"; 
+    // display the picture of dice which selected
     diceDom.src = 'dice-' + diceNumber + '.png'; 
+
+    // increment number if it is not equal to 1
+    if(diceNumber !== 1){
+        // add number to the current roundScore
+        roundScore = roundScore + diceNumber;
+        document.getElementById('current-' + activerPlayer).textContent = roundScore;
+    }else{
+        // if 1
+        roundScore = 0;
+        document.getElementById('current-' + activerPlayer).textContent = 0;
+        
+        activerPlayer === 0 ? (activerPlayer = 1) : (activerPlayer = 0);
+        
+        // swith red dot
+        document.querySelector('.player-0-panel').classList.toggle("active");
+        document.querySelector('.player-1-panel').classList.toggle("active");
+        
+        // hide dice
+        diceDom.style.display = "none"; 
+
+        // if(activerPlayer === 0){
+        //     activerPlayer = 1;
+        // }else{
+        //     activerPlayer = 0;
+        // }
+        
+
+    }
+
 });
 
 
