c88352ec2fd94c6ead0d3fa28a212118b96294e7
tseye <tserendorj.tumen@gmail.com>
2021-04-25 17:55:30 +0800

    shoog shiddeg bolgow

--- app.js
+++ app.js
@@ -5,14 +5,23 @@ var scores = [0, 0];
 // Тоглогчийн ээлжиндээ цуглуулж байгаа оноог хадгалах хувьсагч
 var roundScore = 0;
 // Шооны аль талаараа буусныг хадгалах хувьсагч, 1-6, санамсаргүй
-var dice = Math.floor(Math.random() * 6) + 1;
+
 
 // prepare for start
-document.querySelector('#score-0').textContent = 0;
-document.querySelector('#score-1').innerHTML = 0;
-document.querySelector('#current-0').innerHTML = 0;
-document.querySelector('#current-1').innerHTML = 0;
-document.querySelector('.dice').style.display = "none"; 
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+var diceDom = document.querySelector('.dice');
+diceDom.style.display = "none";
+
+document.querySelector('.btn-roll').addEventListener('click', function(){
+    var diceNumber = Math.floor(Math.random() * 6) + 1;
+    diceDom.style.display = "block"; 
+    diceDom.src = 'dice-' + diceNumber + '.png'; 
+});
+
 
 // Тоглогчдын цуглуулсан оноог хадгалах хувьсагч
 
